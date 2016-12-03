dc9f07e548f1a58684b8f8dec18a2eeb7294604e
petecass <pete@petecss.com>
2016-12-03 23:03:53 +0800

    adds devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  # # Overide default devise controllers
+  # devise_for :users, controllers: { registrations: 'registrations',
+  #                                   sessions: 'sessions',
+  #                                   confirmations: 'confirmations',
+  #                                   unlocks: 'unlocks' }
 
   root 'static_pages#index'
 end
